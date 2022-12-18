880a32c39b278fd8be8582c587d241ef75c0b27b
zain ali <mzain42401@gmail.com>
2022-12-18 14:30:50 +0500

    practice

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    
+      <nav>
+        <div>
+            <h1>MZ Developer</h1>
+        </div>
+        <div>
+            <ul>
+                <li>contact</li>
+                <li>profile</li>
+                <li>task</li>
+                <li>project</li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
